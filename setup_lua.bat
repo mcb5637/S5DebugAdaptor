4699616e07740a84b9c5b9c897445d07ea0a80ab
mcb <mgrbo@web.de>
2026-07-20 17:29:00 +0200

    fix ci

--- setup_lua.bat
+++ setup_lua.bat
@@ -11,5 +11,5 @@ if exist "%out%\" (
     del %temparch%
     move /Y %out%\include\* %out%
     del %out%\include\
-    move /Y \s5_lua\S5Lua5.lib %out%\S5Lua5.lib
+    copy /Y .\s5_lua\S5Lua5.lib .\S5DebugAdaptor\luapp\lua\lua50\
 )

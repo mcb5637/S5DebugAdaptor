f9a4380e1eb05c15028522cc640efcbe31e07640
schmeling65 <markusrumpel2@gmail.com>
2026-07-25 20:09:33 +0200

    debugadaptor built

--- extension/src/debugadaptor/debugadaptor.ts
+++ extension/src/debugadaptor/debugadaptor.ts
@@ -1,16 +1,50 @@
-import * as vscode from 'vscode'
-import { checkProcess } from './findprocess';
+import * as vscode from "vscode";
+import { checkProcess } from "./findprocess.ts";
+import { S5DebugAdapterDescriptorTrackerFactory } from "./debugadaptortracker.ts";
 
-type timeoutTrigger = NodeJS.Timeout | undefined
+type timeoutTrigger = NodeJS.Timeout | undefined;
 
 export class S5DebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-	private searchIntervall: timeoutTrigger = undefined
-	constructor() {
-	}
-	public async createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): Promise<vscode.ProviderResult<vscode.DebugAdapterDescriptor>> {
+  private searchIntervall: timeoutTrigger = undefined;
+  private debugAdaptorTracker: S5DebugAdapterDescriptorTrackerFactory;
+  private debugAdaptorTrackerDisposable: vscode.Disposable;
+
+  public debugConfiguration: vscode.DebugConfiguration;
+  public isDebuggingActive: boolean = false;
+
+  constructor(extensionContext: vscode.ExtensionContext) {
+    this.debugConfiguration = {
+      type: "s5lua",
+      request: "launch",
+      name: "Webview Debug Session",
+    };
+    this.debugAdaptorTracker = new S5DebugAdapterDescriptorTrackerFactory(this);
+    this.debugAdaptorTrackerDisposable = vscode.debug.registerDebugAdapterTrackerFactory('*', this.debugAdaptorTracker)
+	  extensionContext.subscriptions.push(this.debugAdaptorTrackerDisposable);
+    extensionContext.subscriptions.push(
+      vscode.debug.onDidTerminateDebugSession((session) => {
+        if (this.isDebuggingActive && session.name === this.debugConfiguration.name) {
+          console.log("onDidTerminateDebugSession triggered!")
+			    /* Auswertung der Abbruch-Ursache
+                if (programmRegulaerBeendet) {
+                    console.log('Das Programm lief regulär zu Ende.');
+                    meinCallback('REGULAR_EXIT');
+                } else {
+                    console.log('Das Debugging wurde manuell vom Nutzer abgebrochen (Stopp-Button).');
+                    meinCallback('MANUAL_STOP');
+                }
+          */
+        }
+      }),
+    );
+  }
+
+  public async createDebugAdapterDescriptor(
+    session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined,
+  ): Promise<vscode.ProviderResult<vscode.DebugAdapterDescriptor>> {
+    /*
 		let conf = session.configuration;
-		
-		/*
 		if (conf.request === "launch" && conf.program) {
 			let del = 1;
 			if (conf.attachDelay) {
@@ -21,20 +55,33 @@ export class S5DebugAdapterDescriptorFactory implements vscode.DebugAdapterDescr
 		}
 		*/
 
-		return new vscode.DebugAdapterServer(19021);
-	}
+    return new vscode.DebugAdapterServer(19021);
+  }
 
-	public searchForGame() {
-		this.searchIntervall =  setInterval( async () =>{
-			if (await checkProcess("settlershok.exe")) {
-				
-			}
-		},2000)
-	}
+  private setDebuggingStatus(status: boolean) {
+	this.isDebuggingActive = status
+  }
 
-	public stopSearchForGame() {
-		console.log("Suchintervall beendet")
-		clearInterval(this.searchIntervall)
-		this.searchIntervall = undefined
-	}
+  private async startDebugging() {
+    console.log("Searching...")
+    if (this.isDebuggingActive && (await checkProcess("settlershok.exe"))) {
+      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+      const didStart = await vscode.debug.startDebugging(workspaceFolder, this.debugConfiguration);
+      if (didStart) {
+        clearInterval(this.searchIntervall);
+        this.setDebuggingStatus(true)
+        return;
+      }
+      setTimeout(this.startDebugging, 2000);
+    }
+  }
+
+  public searchForGame() {
+    this.searchIntervall = setTimeout(() => {this.startDebugging()}, 2000);
+  }
+
+  public stopSearchForGame() {
+    clearInterval(this.searchIntervall);
+    this.searchIntervall = undefined;
+  }
 }
